aa04a79fc4381f4abf0b03a993e30ab71443e623
unknown <niaridani@gmail.com>
2023-03-16 11:06:41 +0800

    laravel model controller

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('todo.index') }}" :active="request()->routeIs('todo')">
+                        {{ __('Todo') }}
+                    </x-nav-link>
                 </div>
             </div>
 
